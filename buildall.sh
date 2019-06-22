15c5735f07026e9c2fd6a3fd0715b43002c1b6a5
Quanhu Sheng <shengqh@gmail.com>
2019-06-21 21:42:24 -0500

    20190621

---
+++ buildall.sh
@@ -0,0 +1,20 @@
+cd /home/shengq2/dockers/r
+sh build.sh
+docker push shengqh/bioinfo:r3.6.0
+
+cd /home/shengq2/dockers/r_python2
+sh build.sh
+docker push shengqh/bioinfo:r3.6.0_python2.7.12
+
+cd /home/shengq2/dockers/mono
+sh build.sh
+docker push shengqh/bioinfo:mono5.20
+
+cd /home/shengq2/dockers/rpmj
+sh build.sh
+docker push shengqh/bioinfo:r_python_mono_jre
+
+cd /home/shengq2/dockers/rnaseq
+sh build.sh
+docker push shengqh/bioinfo:rnaseq                                                                                                                                                                                                        
+
