1060cd599198bcc38c8b68657850ac39c23a5db3
Quanhu Sheng <shengqh@gmail.com>
2019-06-24 09:26:36 -0500

    python3

--- buildall.sh
+++ buildall.sh
@@ -2,6 +2,14 @@ cd /home/shengq2/dockers/r
 sh build.sh
 docker push shengqh/bioinfo:r3.6.0
 
+cd /home/shengq2/dockers/r_python3
+sh build.sh
+docker push shengqh/bioinfo:r3.6.0_python3.7.3
+
+cd /home/shengq2/dockers/annotateGenome
+sh build.sh
+docker push shengqh/bioinfo:annotateGenome
+
 cd /home/shengq2/dockers/r_python2
 sh build.sh
 docker push shengqh/bioinfo:r3.6.0_python2.7.12
---
+++ r_python3/build.sh
@@ -0,0 +1,1 @@
+docker build -t shengqh/bioinfo:r3.6.0_python3.7.3 .
