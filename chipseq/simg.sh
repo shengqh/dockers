1d1abd9cf7013453857d3d596908744ea1436ab5
Quanhu Sheng <shengqh@gmail.com>
2019-08-16 16:15:35 -0500

    chipseq

--- chipseq/simg.sh
+++ chipseq/simg.sh
@@ -1,3 +1,3 @@
-singularity build cqs-exomeseq.simg docker://shengqh/bioinfo:cqs-exomeseq
+singularity build cqs-chipseq.simg docker://shengqh/bioinfo:cqs-chipseq
 
-bbcp -f -w 4m --port 5031 -s 16 cqs-exomeseq.simg shengq2@cqs3.accre.vanderbilt.edu:/scratch/cqs/softwares/singularity
+bbcp -f -w 4m --port 5031 -s 16 cqs-chipseq.simg shengq2@cqs3.accre.vanderbilt.edu:/scratch/cqs/softwares/singularity
---
+++ r_python2 copy/build.sh
@@ -0,0 +1,1 @@
+docker build -t shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python2.7.12 .
