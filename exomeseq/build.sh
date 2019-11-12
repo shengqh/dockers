c0614cc846166e82c209e55069161e60e7f90dca
Quanhu Sheng <shengqh@gmail.com>
2019-11-12 09:52:18 -0600

    20191112

--- exomeseq/build.sh
+++ exomeseq/build.sh
@@ -1,2 +1,5 @@
-docker build -t shengqh/bioinfo:cqs-exomeseq .
-docker push shengqh/bioinfo:cqs-exomeseq
+date_string=`date +%Y%m%d`
+docker build -t shengqh/cqs_exomeseq:${date_string} .
+docker build -t shengqh/cqs_exomeseq:latest .
+docker push shengqh/cqs_exomeseq:${date_string}
+docker push shengqh/cqs_exomeseq:latest
--- exomeseq/simg.sh
+++ exomeseq/simg.sh
@@ -1,3 +1,3 @@
-singularity build cqs-exomeseq.simg docker://shengqh/bioinfo:cqs-exomeseq
+singularity build cqs-exomeseq.simg docker://shengqh/cqs_exomeseq:latest
 
 bbcp -f -w 4m --port 5031 -s 16 cqs-exomeseq.simg shengq2@cqs3.accre.vanderbilt.edu:/scratch/cqs/softwares/singularity
