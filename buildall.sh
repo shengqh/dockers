d70c67dffab4d6c68d2c6ba29397161e5ee2d7b1
Quanhu Sheng <shengqh@gmail.com>
2020-04-01 10:44:47 -0500

    v20200401

--- buildall.sh
+++ buildall.sh
@@ -1,26 +1,30 @@
-cd /home/shengq2/programs/dockers/perl
-sh build.sh
-docker push shengqh/bioinfo:java1.8.0_perl5.22.1
+# cd /home/shengq2/programs/dockers/perl
+# sh build.sh
+# docker push shengqh/bioinfo:java1.8.0_perl5.22.1
 
-cd /home/shengq2/programs/dockers/r
-sh build.sh
-docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1
+# cd /home/shengq2/programs/dockers/r
+# sh build.sh
+# docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1
 
-cd /home/shengq2/programs/dockers/r_python3
-sh build.sh
-docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python3.7.3
+# cd /home/shengq2/programs/dockers/r_python3
+# sh build.sh
+# docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python3.7.3
 
-cd /home/shengq2/programs/dockers/annotateGenome
-sh build.sh
-docker push shengqh/bioinfo:annotateGenome
+# cd /home/shengq2/programs/dockers/r_python2
+# sh build.sh
+# docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python2.7.12
 
-cd /home/shengq2/programs/dockers/rpjm
-sh build.sh
-docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python3.7.3_mono5.20.1.19
+# cd /home/shengq2/programs/dockers/annotateGenome
+# sh build.sh
+# docker push shengqh/bioinfo:annotateGenome
 
-cd /home/shengq2/programs/dockers/base
-sh build.sh
-docker push shengqh/bioinfo:base                                                                                                                                                                                                        
+# cd /home/shengq2/programs/dockers/rpjm
+# sh build.sh
+# docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python3.7.3_mono5.20.1.19
+
+# cd /home/shengq2/programs/dockers/base
+# sh build.sh
+# docker push shengqh/bioinfo:base                                                                                                                                                                                                        
 
 cd /home/shengq2/programs/dockers/rnaseq
 sh build.sh
@@ -41,8 +45,4 @@ cd /home/shengq2/programs/dockers/cqs-gatk
 sh build.sh
 docker push shengqh/bioinfo:cqs-gatk4                                                                                                                                                                                                       
 sh simg.sh
-
-cd /home/shengq2/programs/dockers/r_python2
-sh build.sh
-docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python2.7.12
 
