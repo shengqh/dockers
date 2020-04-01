95da1eedd5b62622cc5f10bcd881c91636080d30
Quanhu Sheng <shengqh@gmail.com>
2020-04-01 14:46:54 -0500

    add cqstools

--- buildall.sh
+++ buildall.sh
@@ -22,18 +22,18 @@
 # sh build.sh
 # docker push shengqh/bioinfo:java1.8.0_perl5.22.1_r3.6.1_python3.7.3_mono5.20.1.19
 
-# cd /home/shengq2/programs/dockers/base
-# sh build.sh
-# docker push shengqh/bioinfo:base                                                                                                                                                                                                        
-
-cd /home/shengq2/programs/dockers/rnaseq
+cd /home/shengq2/programs/dockers/base
 sh build.sh
-docker push shengqh/bioinfo:cqs-rnaseq                                                                                                                                                                                                        
-sh simg.sh
+docker push shengqh/bioinfo:base                                                                                                                                                                                                        
 
 cd /home/shengq2/programs/dockers/exomeseq
 sh build.sh
 docker push shengqh/bioinfo:cqs-exomeseq                                                                                                                                                                                                        
+sh simg.sh
+
+cd /home/shengq2/programs/dockers/rnaseq
+sh build.sh
+docker push shengqh/bioinfo:cqs-rnaseq                                                                                                                                                                                                        
 sh simg.sh
 
 cd /home/shengq2/programs/dockers/smallRNA
