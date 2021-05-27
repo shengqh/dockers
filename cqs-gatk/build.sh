48d3beb36d915ef61cbd450d308284c9831e97ab
Quanhu Sheng <shengqh@gmail.com>
2021-05-26 22:59:21 -0500

    gatk 4.2.0

--- cqs-gatk/build.sh
+++ cqs-gatk/build.sh
@@ -1,5 +1,5 @@
 date_string=`date +%Y%m%d`
-gatk_version='4.1.8.1'
+gatk_version='4.2.0.0'
 docker build -t shengqh/cqs_gatk${gatk_version}:${date_string} .
 docker build -t shengqh/cqs_gatk${gatk_version}:latest .
 docker push shengqh/cqs_gatk${gatk_version}:${date_string}
