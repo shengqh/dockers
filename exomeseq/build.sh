ce1b1c992783a14b26ed1aa75c5583301b2d9a8b
Quanhu Sheng <shengqh@gmail.com>
2021-10-20 10:00:41 -0500

    Exomeseq complexheatmap

--- exomeseq/build.sh
+++ exomeseq/build.sh
@@ -1,5 +1,3 @@
 date_string=`date +%Y%m%d`
 docker build -t shengqh/cqs_exomeseq:${date_string} .
 docker build -t shengqh/cqs_exomeseq:latest .
-docker push shengqh/cqs_exomeseq:${date_string}
-docker push shengqh/cqs_exomeseq:latest
---
+++ exomeseq/push.sh
@@ -0,0 +1,3 @@
+date_string=`date +%Y%m%d`
+docker push shengqh/cqs_exomeseq:${date_string}
+docker push shengqh/cqs_exomeseq:latest
