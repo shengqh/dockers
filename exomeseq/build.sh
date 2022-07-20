2a63fbb7e868d04a7aaabc767ea2ededd67265e2
Quanhu Sheng <shengqh@gmail.com>
2022-07-19 23:52:30 -0500

    v20220719

--- exomeseq/build.sh
+++ exomeseq/build.sh
@@ -1,3 +1,3 @@
 date_string=`date +%Y%m%d`
-docker build -t shengqh/cqs_exomeseq:${date_string} .
+docker build --progress=plain -f Dockerfile.20220719 -t shengqh/cqs_exomeseq:${date_string} .
 docker tag shengqh/cqs_exomeseq:${date_string} shengqh/cqs_exomeseq:latest
