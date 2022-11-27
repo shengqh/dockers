e6d857f2ed854dcd82ee0097c27f53c3cad553c3
Quanhu Sheng <shengqh@gmail.com>
2022-11-27 13:19:18 -0600

    v20221127

--- buildall.sh
+++ buildall.sh
@@ -1,32 +1,36 @@
-# cd /home/shengq2/programs/dockers/annotateGenome
+# cd /home/shengq2/program/dockers/annotateGenome
 # sh build.sh
 # docker push shengqh/bioinfo:annotateGenome
 
-#cd /home/shengq2/programs/dockers/ubuntu_rjpmp
+#cd /home/shengq2/program/dockers/ubuntu_rjpmp
 #sh build.sh
-#docker push shengqh/bioinfo:ubuntu20.04_r4.0_java1.8_perl5.30_mono6.12_python3.8
+#sh push.sh
 
-cd /home/shengq2/programs/dockers/base
+cd /home/shengq2/program/dockers/base
 sh build.sh
-docker push shengqh/bioinfo:base                                                                                                                                                                                                        
+sh push.sh
 
-cd /home/shengq2/programs/dockers/exomeseq
+cd /home/shengq2/program/dockers/chipseq
 sh build.sh
-docker push shengqh/bioinfo:cqs-exomeseq                                                                                                                                                                                                        
-sh simg.sh
+sh push.sh
 
-cd /home/shengq2/programs/dockers/rnaseq
-sh build.sh
-docker push shengqh/bioinfo:cqs-rnaseq                                                                                                                                                                                                        
-sh simg.sh
+# cd /home/shengq2/program/dockers/exomeseq
+# sh build.sh
+# docker push shengqh/bioinfo:cqs-exomeseq                                                                                                                                                                                                        
+# sh simg.sh
 
-cd /home/shengq2/programs/dockers/smallRNA
-sh build.sh
-docker push shengqh/bioinfo:cqs-smallRNA                                                                                                                                                                                                       
-sh simg.sh
+# cd /home/shengq2/program/dockers/rnaseq
+# sh build.sh
+# docker push shengqh/bioinfo:cqs-rnaseq                                                                                                                                                                                                        
+# sh simg.sh
 
-cd /home/shengq2/programs/dockers/cqs-gatk
-sh build.sh
-docker push shengqh/bioinfo:cqs-gatk4                                                                                                                                                                                                       
-sh simg.sh
+# cd /home/shengq2/program/dockers/smallRNA
+# sh build.sh
+# docker push shengqh/bioinfo:cqs-smallRNA                                                                                                                                                                                                       
+# sh simg.sh
+
+# cd /home/shengq2/program/dockers/cqs-gatk
+# sh build.sh
+# docker push shengqh/bioinfo:cqs-gatk4                                                                                                                                                                                                       
+# sh simg.sh
 
--- ubuntu20_rjpmp/build.sh
+++ ubuntu20_rjpmp/build.sh
@@ -1,3 +1,3 @@
-#date_string=`date +%Y%m%d`
-docker build --progress=plain -t shengqh/ubuntu20.04_r4.2.2_java11.0.17_perl5.30.0_mono6.12.0.182_python3.8.10 .
-
+date_string=`date +%Y%m%d`
+docker build --progress=plain -t shengqh/ubuntu20.04_r4.2.2_java11.0.17_perl5.30.0_mono6.12.0.182_python3.8.10:${date_string} .
+docker tag shengqh/ubuntu20.04_r4.2.2_java11.0.17_perl5.30.0_mono6.12.0.182_python3.8.10:${date_string} shengqh/ubuntu20.04_r4.2.2_java11.0.17_perl5.30.0_mono6.12.0.182_python3.8.10:latest
