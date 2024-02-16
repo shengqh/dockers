55e1335d8e4593572b2813530f98a017dcb4c957
Quanhu Sheng <shengqh@gmail.com>
2024-02-16 14:53:56 -0600

    gatk4.5.0.0

--- cqs-gatk/build.sh
+++ cqs-gatk/build.sh
@@ -1,4 +1,4 @@
 date_string=`date +%Y%m%d`
-gatk_version='4.2.2.0'
-docker build -t shengqh/cqs_gatk${gatk_version}:${date_string} .
-docker build -t shengqh/cqs_gatk${gatk_version}:latest .
+gatk_version='4.5.0.0'
+docker build --progress=plain -t shengqh/cqs_gatk${gatk_version}:${date_string} .
+docker tag shengqh/cqs_gatk${gatk_version}:${date_string} shengqh/cqs_gatk${gatk_version}:latest
--- cqs-gatk/push.sh
+++ cqs-gatk/push.sh
@@ -1,4 +1,4 @@
 date_string=`date +%Y%m%d`
-gatk_version='4.2.2.0'
+gatk_version='4.5.0.0'
 docker push shengqh/cqs_gatk${gatk_version}:${date_string}
 docker push shengqh/cqs_gatk${gatk_version}:latest
