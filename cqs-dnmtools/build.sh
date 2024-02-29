5074c77def9802906bd8418023f06189e79481ff
Quanhu Sheng <shengqh@gmail.com>
2024-02-28 23:51:12 -0600

    update build command

--- cqs-dnmtools/build.sh
+++ cqs-dnmtools/build.sh
@@ -1,3 +1,3 @@
 date_string=`date +%Y%m%d`
-docker build -t shengqh/cqs_dnmtools:${date_string} .
-docker build -t shengqh/cqs_dnmtools:latest .
+docker build --progress=plain -t shengqh/cqs_dnmtools:${date_string} .
+docker tag shengqh/cqs_dnmtools:${date_string} shengqh/cqs_dnmtools:latest
