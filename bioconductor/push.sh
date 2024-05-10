ffcf111e5c7ff09420b044a45c3c46b4298b207d
Quanhu Sheng <shengqh@gmail.com>
2024-05-10 13:58:34 -0500

    cqs_bioconductor_3_19

--- bioconductor/push.sh
+++ bioconductor/push.sh
@@ -1,3 +1,3 @@
 date_string=`date +%Y%m%d`
-docker push shengqh/cqs_bioconductor_3_18:${date_string}
-docker push shengqh/cqs_bioconductor_3_18:latest
+docker push shengqh/cqs_bioconductor_3_19:${date_string}
+docker push shengqh/cqs_bioconductor_3_19:latest
