3ca9c5e979e019d3ab5e12646e78c85907f48550
Quanhu Sheng <shengqh@gmail.com>
2024-08-12 16:52:13 -0500

    v20240812

--- debian_r_python_java_mono/build.sh
+++ debian_r_python_java_mono/build.sh
@@ -1,4 +1,4 @@
 date_string=`date +%Y%m%d`
-docker_name='shengqh/debian10.2.1_r4.3.0_python3.10.11_java11.0.18_mono6.12.0.182'
+docker_name='shengqh/debian12bookworm_r4.4.1_python3.12.4_java17.0.11_mono6.12.0.200'
 docker build --progress=plain -t ${docker_name}:${date_string} .
 docker tag ${docker_name}:${date_string} ${docker_name}:latest
