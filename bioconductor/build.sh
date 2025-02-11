678f4a9425c5da5eaa2462bea0bfad5525e38d14
Quanhu Sheng <shengqh@gmail.com>
2025-02-11 14:32:21 -0600

    bioconductor 320

--- bioconductor/build.sh
+++ bioconductor/build.sh
@@ -1,3 +1,3 @@
 date_string=`date +%Y%m%d`
-docker build --progress=plain -t shengqh/cqs_bioconductor_3_19:${date_string} .
-docker tag shengqh/cqs_bioconductor_3_19:${date_string} shengqh/cqs_bioconductor_3_19:latest
+docker build --progress=plain -t shengqh/cqs_bioconductor_3_20_r442:${date_string} .
+docker tag shengqh/cqs_bioconductor_3_20_r442:${date_string} shengqh/cqs_bioconductor_3_20_r442:latest
