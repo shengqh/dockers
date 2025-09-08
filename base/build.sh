ac53af4cadd59e14bfcc1ea0ee613b76c011267c
Quanhu Sheng <shengqh@gmail.com>
2025-09-08 10:37:04 -0500

    update base using R 4.5.1

--- base/build.sh
+++ base/build.sh
@@ -1,3 +1,3 @@
 date_string=`date +%Y%m%d`
-docker build --progress=plain -t shengqh/bioinfo_base:${date_string} .
+docker buildx build --progress=plain -t shengqh/bioinfo_base:${date_string} .
 docker tag shengqh/bioinfo_base:${date_string} shengqh/bioinfo_base:latest
