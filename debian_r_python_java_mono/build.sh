677c4a7cbe4aa49d5e040eb5e2d04c9dd7153e5c
Quanhu Sheng <shengqh@gmail.com>
2025-09-08 10:36:38 -0500

    debian12bookworm_r4.5.1_python3.13.7_java17.0.16_mono6.12.0.200

--- debian_r_python_java_mono/build.sh
+++ debian_r_python_java_mono/build.sh
@@ -1,4 +1,4 @@
 date_string=`date +%Y%m%d`
-docker_name='shengqh/debian12bookworm_r4.4.1_python3.12.4_java17.0.11_mono6.12.0.200'
-docker buildx build --progress=plain -t ${docker_name}:${date_string} .
+docker_name='shengqh/debian12bookworm_r4.5.1_python3.13.7_java17.0.16_mono6.12.0.200'
+docker buildx build --progress plain -t ${docker_name}:${date_string} .
 docker tag ${docker_name}:${date_string} ${docker_name}:latest
--- debian_r_python_java_mono/push.sh
+++ debian_r_python_java_mono/push.sh
@@ -1,4 +1,4 @@
 date_string=`date +%Y%m%d`
-docker_name='shengqh/debian12bookworm_r4.4.1_python3.12.4_java17.0.11_mono6.12.0.200'
+docker_name='shengqh/debian12bookworm_r4.5.1_python3.13.7_java17.0.16_mono6.12.0.200'
 docker push ${docker_name}:${date_string}
 docker push ${docker_name}:latest
