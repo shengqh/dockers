646fef821377362c7b3fbeeffd52fedea3ab03d8
Quanhu Sheng <shengqh@gmail.com>
2025-11-28 12:56:43 -0600

    v20251128

--- PRS/build.sh
+++ PRS/build.sh
@@ -1,3 +1,3 @@
 date_string=`date +%Y%m%d`
-docker build --progress=plain -t shengqh/prs:${date_string} .
+docker buildx build --progress=plain -t shengqh/prs:${date_string} .
 docker tag shengqh/prs:${date_string} shengqh/prs:latest
