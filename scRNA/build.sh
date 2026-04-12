ae5dc401fe464fe55f688a825a8ad2ecaf7f5c5d
Quanhu Sheng <shengqh@gmail.com>
2026-04-12 01:04:18 -0400

    add mecell into scrna

--- scRNA/build.sh
+++ scRNA/build.sh
@@ -1,4 +1,11 @@
 date_string=`date +%Y%m%d`
+
+if [[ ! -d "MEcell_python" ]]; then
+  git clone git@github.com:shengqh/MEcell_python.git
+else
+  cd MEcell_python && git pull && cd ..
+fi
+
 docker buildx build --progress=plain -t shengqh/cqs_scrnaseq:${date_string} .
 docker image rm -f shengqh/cqs_scrnaseq:latest
 docker tag shengqh/cqs_scrnaseq:${date_string} shengqh/cqs_scrnaseq:latest
